e9983505bc63ff48c9a7bcdace97de46d39cd88a
Pablo Gonzalez <pdonaire1@gmail.com>
2016-07-24 23:13:59 -0430

    os in main

--- medical_box_api/wsgi.py
+++ medical_box_api/wsgi.py
@@ -24,7 +24,9 @@ application = Cling(get_wsgi_application())
 """
 
 # source: https://djangogirls.gitbooks.io/django-girls-tutorial-extensions/content/heroku/
+import os
 from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "medical_box_api.settings")
 from dj_static import Cling
 application = Cling(get_wsgi_application())
 from whitenoise.django import DjangoWhiteNoise
