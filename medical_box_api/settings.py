df0a23a3fcdd5fd47b724a956efc68c410b50a22
Pablo Gonzalez <pdonaire1@gmail.com>
2016-07-24 19:58:30 -0430

    Heroku config

--- medical_box_api/settings.py
+++ medical_box_api/settings.py
@@ -10,6 +10,28 @@ https://docs.djangoproject.com/en/1.9/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
+########## HEROKU ###########
+import dj_database_url
+db_from_env = dj_database_url.config() #db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+
+#static files
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+###########END HEROKU #######
 import datetime
 import os
 import os
