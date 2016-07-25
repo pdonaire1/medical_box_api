42f10cc544e9897ecc9829f040143e20e0f9ea7f
Pablo Gonzalez <pdonaire1@gmail.com>
2016-07-24 21:23:55 -0430

    more configurations

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn
+web: python manage.py runserver 0.0.0.0:$PORT --noreload
--- medical_box_api/wsgi.py
+++ medical_box_api/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
+""" Original ->
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "medical_box_api.settings")
 
 application = get_wsgi_application()
+"""
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
+
